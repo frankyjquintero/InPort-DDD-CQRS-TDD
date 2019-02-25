a9d247f9d15bde8b6560356b60334f855fd2346b
FrankyJQuintero <frankyjquintero@unicesar.edu.co>
2019-02-25 00:30:54 -0500

    Interfaz para Bus

---
+++ Inport/InPort.Domain.Core/Bus/IMediatorHandler.cs
@@ -0,0 +1,12 @@
+﻿using InPort.Domain.Core.Commands;
+using InPort.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace InPort.Domain.Core.Bus
+{
+    public interface IMediatorHandler
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        Task RaiseEvent<T>(T @event) where T : Event;
+    }
+}
