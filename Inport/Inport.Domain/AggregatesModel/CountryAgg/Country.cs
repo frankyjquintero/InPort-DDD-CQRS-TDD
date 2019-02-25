d610285294407069dff29b0069b1a8d50ff28c27
FrankyJQuintero <frankyjquintero@unicesar.edu.co>
2019-02-25 00:34:51 -0500

    Country Agregate

---
+++ Inport/Inport.Domain/AggregatesModel/CountryAgg/Country.cs
@@ -0,0 +1,40 @@
+﻿
+using InPort.Domain.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InPort.Domain.AggregatesModel.CountryAgg
+{
+    public class Country
+        : Entity
+    {
+        #region Properties
+
+
+        public string CountryName { get; private set; }
+
+        public string CountryISOCode { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        //requerido por EF
+        public Country() { }
+
+        public Country(string countryName, string countryISOCode)
+        {
+            if (String.IsNullOrWhiteSpace(countryName))
+                throw new ArgumentNullException("countryName");
+
+            if (String.IsNullOrWhiteSpace(countryISOCode))
+                throw new ArgumentNullException("countryISOCode");
+
+            this.CountryName = countryName;
+            this.CountryISOCode = countryISOCode;
+        }
+
+        #endregion
+    }
+}
---
+++ Inport/Inport.Domain/AggregatesModel/CountryAgg/ICountryRepository.cs
@@ -0,0 +1,12 @@
+﻿using InPort.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InPort.Domain.AggregatesModel.CountryAgg
+{
+    public interface ICountryRepository
+        : IRepository<Country>
+    {
+    }
+}
