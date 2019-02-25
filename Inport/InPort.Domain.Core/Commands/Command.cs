a35b201c422b27c59719474fc5babb6c7cca69a3
FrankyJQuintero <frankyjquintero@unicesar.edu.co>
2019-02-25 00:31:51 -0500

    abstract class para Comandos

---
+++ Inport/InPort.Domain.Core/Commands/Command.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using InPort.Domain.Core.Events;
+using FluentValidation.Results;
+
+namespace InPort.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public abstract bool IsValid();
+    }
+}
