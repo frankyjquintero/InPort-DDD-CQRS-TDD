60087c18133568b52f8b7f559e09076aad63c57d
FrankyJQuintero <frankyjquintero@unicesar.edu.co>
2019-02-25 00:35:30 -0500

    MeasurentUnit Agregate

---
+++ Inport/Inport.Domain/AggregatesModel/MeasurentUnitAgg/IMeasurentUnitRepository.cs
@@ -0,0 +1,13 @@
+﻿using InPort.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InPort.Domain.AggregatesModel.MeasurentUnitAgg
+{
+    public interface IMeasurentUnitRepository
+        : IRepository<MeasurentUnit>
+    {
+
+    }
+}
---
+++ Inport/Inport.Domain/AggregatesModel/MeasurentUnitAgg/MeasurentUnit.cs
@@ -0,0 +1,34 @@
+﻿using InPort.Domain.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InPort.Domain.AggregatesModel.MeasurentUnitAgg
+{
+    public class MeasurentUnit
+        : Entity
+    {
+        #region Constructor
+        public MeasurentUnit()
+        {
+        }
+
+        public MeasurentUnit(string measurentUnitType, string measurentUnitDescription, string[] measurentUnitDetails)
+        {
+            MeasurentUnitType = measurentUnitType;
+            MeasurentUnitDescription = measurentUnitDescription;
+            MeasurentUnitDetails = measurentUnitDetails;
+        }
+        #endregion
+        #region Properties
+
+
+        public string MeasurentUnitType { get; private set; }
+
+        public string MeasurentUnitDescription { get; private set; }
+
+        public string[] MeasurentUnitDetails { get; private set; }
+
+        #endregion
+    }
+}
