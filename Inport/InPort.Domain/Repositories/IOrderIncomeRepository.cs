95538e83750c9b49bd908572d5f5847eb50888c0
FrankyJQuintero <frankyjquintero@unicesar.edu.co>
2019-03-09 14:49:15 -0500

    Fixed

--- Inport/InPort.Domain/Repositories/IOrderIncomeRepository.cs
+++ Inport/InPort.Domain/Repositories/IOrderIncomeRepository.cs
@@ -4,7 +4,7 @@ using InPort.Domain.Core.IRepository;
 namespace InPort.Domain.Repositories
 {
     /// <summary>
-    /// El contrato para el repositorio de ordenes de Ingreso
+    /// El contrato para el repositorio de ordenes de Longitud
     /// </summary>
     public interface IOrderIncomeRepository
         : IReadRepository<OrderIncome>, ICreateRepository<OrderIncome>, IRemoveRepository<OrderIncome>, IUpdateRepository<OrderIncome>
