b8fe806c9dc08f5af65c009be1a43eec9292d318
FrankyJQuintero <frankyjquintero@unicesar.edu.co>
2019-03-09 14:49:47 -0500

    Fixed context

--- Inport/InPort.Infra.Data/Context/InPortContext.cs
+++ Inport/InPort.Infra.Data/Context/InPortContext.cs
@@ -1,6 +1,9 @@
 ﻿using System.IO;
 using InPort.Domain.AggregatesModel.CountryAgg;
 using InPort.Domain.AggregatesModel.CustomerAgg;
+using InPort.Domain.AggregatesModel.MeasurentUnitAgg;
+using InPort.Domain.AggregatesModel.OrderIncomeAgg;
+using InPort.Domain.AggregatesModel.ProductAgg;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -15,10 +18,15 @@ namespace InPort.Infra.Data.Context
 
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<MeasurentUnit> MeasurentUnits { get; set; }
+        public DbSet<OrderIncome> OrderIncomes { get; set; }
+        public DbSet<OrderIncomeLine> OrderIncomeLines { get; set; }
+        public DbSet<OrderIncomeStatus> OrderStatus { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(InPortDbContext).Assembly);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(InPortDbContext).Assembly);         
         }
     }
 }
