74dd2c064ba7d68e5f6cd7a07922df84b0616b1d
FrankyJQuintero <frankyjquintero@unicesar.edu.co>
2019-03-09 21:48:51 -0500

    Country Factory

---
+++ Inport/InPort.Domain/AggregatesModel/CountryAgg/CountryFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InPort.Domain.AggregatesModel.CountryAgg
+{
+    public static class CountryFactory
+    {
+        public static Country CreateCountry(string countryName, string isoCode)
+        {
+            //crear nueva instancia y establecer identidad
+            var country = new Country(countryName, isoCode);
+
+            country.GenerateNewIdentity();
+        
+
+            return country;
+        }
+    }
+}
