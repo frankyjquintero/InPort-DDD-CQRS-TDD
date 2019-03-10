992c974e69d2f6d90f5c4056a1c9ca42d7fd6b36
FrankyJQuintero <frankyjquintero@unicesar.edu.co>
2019-03-09 21:49:02 -0500

    MeasurentUnit Factory

---
+++ Inport/InPort.Domain/AggregatesModel/MeasurentUnitAgg/MeasurentUnitFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InPort.Domain.AggregatesModel.MeasurentUnitAgg
+{
+    public static class MeasurentUnitFactory
+    {
+        public static MeasurentUnit CreateMeasurentUnit(MeasurentUnitType measurentUnitType, string name)
+        {
+            //crear nueva instancia y establecer identidad
+            var measurentUnit = new MeasurentUnit(measurentUnitType, name);
+
+            measurentUnit.GenerateNewIdentity();
+
+            return measurentUnit;
+        }
+    }
+}
