83405e2b48eb1bdd6054a4a22dd762171336fdcb
FrankyJQuintero <frankyjquintero@unicesar.edu.co>
2019-03-09 21:49:42 -0500

    Product fixed an factory

---
+++ Inport/InPort.Domain/AggregatesModel/ProductAgg/ProductFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InPort.Domain.AggregatesModel.ProductAgg
+{
+    public static class ProductFactory
+    {
+        public static Product CreateProduct(string name, string description)
+        {
+            //crear nueva instancia y establecer identidad
+            var product = new Product(name, description);
+
+            product.GenerateNewIdentity();
+
+
+            return product;
+        }
+    }
+}
--- Inport/Inport.Domain/AggregatesModel/ProductAgg/Product.cs
+++ Inport/Inport.Domain/AggregatesModel/ProductAgg/Product.cs
@@ -14,11 +14,18 @@ namespace InPort.Domain.AggregatesModel.ProductAgg
         public string Name { get; private set; }
 
         public string Description { get; private set; }
+        public  ICollection<ProductMeasurentUnit> ProductMeasurentUnits { get; private set; }
 
-    
-        public virtual ICollection<ProductMeasurentUnit> ProductMeasurentUnits { get; private set; }
+        public  ICollection<OrderIncomeLine> OrderIncomeLines { get; private set; }
 
-        public virtual ICollection<OrderIncomeLine> OrderIncomeLines { get; private set; }
+        #endregion
+
+        #region Constructor
+        public Product(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
 
         public Product()
         {
@@ -26,6 +33,8 @@ namespace InPort.Domain.AggregatesModel.ProductAgg
             OrderIncomeLines = new HashSet<OrderIncomeLine>();
         }
         #endregion
+
+
 
 
 
